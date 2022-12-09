b4ec57c1577bb98a08ade84f9565ed230f0dca39
SultanSarygulov <itsme.kaan.sarygulov@gmail.com>
2022-12-09 23:30:04 +0600

    fixed layout

--- app/src/main/java/com/example/greenwichregistr/LoginFragment.kt
+++ app/src/main/java/com/example/greenwichregistr/LoginFragment.kt
@@ -1,11 +1,13 @@
 package com.example.greenwichregistr
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.greenwichregistr.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -24,5 +26,39 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setRegisterButton()
+
+        binding.loginButton.setOnClickListener {
+
+            checkInput()
+
+            colorButton()
+        }
+    }
+
+    private fun colorButton() {
+        if (binding.enterNameEt.text.isEmpty() || binding.enterPnoneEt.text.isEmpty()){
+            binding.loginButton.backgroundTintList =
+                getResources().getColorStateList(R.color.gray)
+        } else {
+            binding.loginButton.backgroundTintList =
+                getResources().getColorStateList(R.color.main_green)
+        }
+    }
+
+    private fun checkInput() {
+        binding.emptyAlertTxt.isVisible =
+            binding.enterNameEt.text.isEmpty() || binding.enterPnoneEt.text.isEmpty()
+
+        binding.emptyNameAlert.isVisible = binding.enterNameEt.text.isEmpty()
+
+        binding.emptyPhoneAlert.isVisible = binding.enterPnoneEt.text.isEmpty()
+    }
+
+    private fun setRegisterButton() {
+        binding.registerTxtButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
     }
 }
