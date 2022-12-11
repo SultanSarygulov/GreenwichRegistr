5fc065ab52ade96c0bc3efe05846e237d6c55d4f
SultanSarygulov <itsme.kaan.sarygulov@gmail.com>
2022-12-12 03:37:18 +0600

    Successfully implemented sending code to phone number

--- app/src/main/java/com/example/greenwichregistr/RegisterFragment.kt
+++ app/src/main/java/com/example/greenwichregistr/RegisterFragment.kt
@@ -3,15 +3,26 @@ package com.example.greenwichregistr
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.greenwichregistr.databinding.FragmentRegisterBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.util.concurrent.TimeUnit
 
 class RegisterFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
 
     private lateinit var binding: FragmentRegisterBinding
 
@@ -20,6 +31,7 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        auth = Firebase.auth
         return binding.root
     }
 
@@ -84,11 +96,38 @@ class RegisterFragment : Fragment() {
 
             binding.emptyPhoneAlert.isVisible = binding.enterPnoneEt.text.isEmpty()
         } else {
-            binding.emptyAlertTxt.isVisible = false
-            findNavController().navigate(R.id.action_registerFragment_to_confirmFragment)
+            sendCode()
         }
 
+    }
 
+    private fun sendCode() {
 
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                Toast.makeText(requireContext(), "Send a correct phone number", Toast.LENGTH_LONG).show()
+                Log.d("Nigger", "${e}")
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                binding.emptyAlertTxt.isVisible = false
+                findNavController().navigate(R.id.action_registerFragment_to_confirmFragment)
+            }
+        }
+
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(binding.enterPnoneEt.text.toString().trim())       // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this.requireActivity())                 // Activity (for callback binding)
+            .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
     }
 }
